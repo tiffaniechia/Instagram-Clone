b6ee7a0bb7a7e710d18419e68d34259b63fa0890
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-04-14 14:16:41 +0100

    basic adding description to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,22 @@ class PostsController < ApplicationController
 
 
   def index
+    @posts = Post.all
   end
   
   def new
+    @post = Post.new
   end
   
   def create
+    @post = Post.new(params[:post].permit(:description))
+    
+    if @post.save
+      redirect_to '/posts'
+    else
+      render 'new'
+    end
+        
   end  
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 
+validates :description, presence: true
 
 
 end
